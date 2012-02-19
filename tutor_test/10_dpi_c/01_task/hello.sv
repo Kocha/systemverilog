0ec91eee9daed7d87a190d0644933b63175455c2
Kocha <suzuki.koichi6121@gmail.com>
2012-02-20 06:15:12 +0900

    add dpi_c task input/output test

---
+++ tutor_test/10_dpi_c/01_task/hello.sv
@@ -0,0 +1,19 @@
+module hello_top;
+   int ret;
+   
+   export "DPI-C" task verilog_task;
+   task verilog_task;
+     input  int i;
+     output int o;
+      #10;
+      $display("Hello from verilog_task()");
+   endtask // verilog_task
+   
+   import "DPI-C" context task c_task(input int i, output int o);
+   
+   initial begin
+      c_task(1, ret);
+      $display("ret=%d", ret);
+   end
+endmodule // hello_top
+
